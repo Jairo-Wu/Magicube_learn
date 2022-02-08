189fee614016b38bb423ebea2263d03297dbab4b
Jairo-Wu <616341451@qq.com>
2022-02-08 17:41:49 +0800

    第一次上传

---
+++ index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="CFOP,魔方,速拧">
+    <meta name="description" content="魔方速拧学习网站、CFOP">    
+    <title>Magicube_Study</title>
+
+    <link rel="shortcut icon" href="./img/cube_icon.png" type="image/x-icon">
+    <link rel="stylesheet" href="./css/common.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body class="back">
+    <?php
+    header("Content-type:text/html;charset=utf-8");  
+    $link=mysqli_connect('localhost','root','password','magicube_study','3306');
+        if(!$link){
+            exit('失败');
+        }
+        else{
+            mysqli_query($link,'SET NAMES UTF8');
+        }
+    ?>
+    <!-- 头部 -->
+    <div class="head"> 
+        <div class="head_icon">
+            <div>
+                <img src="./img/cube_icon.png" alt="" width="80px" height="80px">
+            </div>
+            <div>
+                <h4 >Magicube</h4>
+                <h4 >Study</h4>
+            </div>
+        </div>
+        <div>
+           <ul>               
+                <li ><a href="#">CROSS</a></li>
+                <li ><a href="#">F2L</a></li>
+                <li ><a href="#">OLL</a></li>
+                <li ><a href="#">PLL</a></li>
+            </ul>  
+        </div>
+    </div>
+    <!-- main -->
+    
+        <div class="border"></div>
+        <div class="side">
+        <?php
+        header("Content-type:text/html;charset=utf-8");  
+
+            for($turn=1;$turn<=4;$turn++)
+            {
+                echo "<div class=\"sidein\">";
+                echo "<ul class=\"ul$turn\">";
+                $sql="select count(*) FROM t$turn";
+                $res=mysqli_query($link,$sql);
+                $arr = mysqli_fetch_array($res,MYSQLI_ASSOC);
+                $count=$arr['count(*)'];
+                for ($x=1; $x<=$count; $x++) 
+                {
+                    $sql="select formula from t$turn where id=$x";
+                    $res=mysqli_query($link,$sql);
+                    $arr = mysqli_fetch_array($res,MYSQLI_ASSOC);
+                    $formula=$arr['formula'];
+                     echo "<li>
+                            <img src=\"./upload/$turn/i ($x).png\" class=\"situation\">
+                            <a ></a>
+                            <div>";
+                            echo "<p>";
+
+                                for($i=0;$arr['formula'][$i];$i++)
+                                {                                    
+                                    $ch=$arr['formula'][$i];
+                                    $ch1=$arr['formula'][$i+1];
+                                    $ch2=$arr['formula'][$i+2];
+                                    if((('a'<= $ch) &&('z'>= $ch))||(('A'<=$ch)&&('Z'>=$ch)))
+                                    {
+                                        if(strcmp($ch1,"2")==0)
+                                        {
+                                            if(strcmp($ch2,"'")==0)
+                                            {
+                                                echo "<img src=\"./upload/formula/$ch"."_$ch1.png\" class=\"formula\">";
+                                                $i+=2;
+                                            }
+                                            else
+                                            {
+                                                echo "<img src=\"./upload/formula/$ch"."2.png\" class=\"formula\">";
+                                                $i++;
+                                            }
+                                        }
+                                        else if(strcmp($ch1,"'")==0)
+                                        {
+                                            echo "<img src=\"./upload/formula/$ch"."_.png\" class=\"formula\">";
+                                            $i++;
+                                        }
+                                        else
+                                            echo "<img src=\"./upload/formula/$ch.png\" class=\"formula\">";                                                                                
+                                    }
+                                    else if($ch=='('||$ch==')'||$ch==' ')
+                                    {
+                                        echo "$ch";
+                                    }
+                                    else 
+                                    {
+                                        echo "$ch$ch1$ch2";
+                                        $i+=2;                                        
+                                    }                                                
+                                }
+                                echo "</p>";
+                                
+                    echo " </div>
+                            </li>";
+                }
+                echo "</ul>";
+                echo "</div>";
+            }
+        ?>
+        </div>     
+    <!-- 魔方 -->
+    <div class="cube">
+        <div class="cube_close" ></div>
+        <iframe src="./cube/index.html" frameborder="0" class="cube_html"  scrolling="no"> </iframe>
+        <img src="./img/fresh.svg" alt="刷新" class="fresh" title="刷新">
+    </div>
+    <!-- 说明书 -->
+    <div class="intr">
+        <div class="intr_close" ></div>
+        <ul>
+            <li><img src="./upload/intr/notation1.gif" alt="" ></li>
+            <li><img src="./upload/intr/notation2.gif" alt="" ></li>
+            <li><img src="./upload/intr/notation3.gif" alt="" ></li>
+            <li><img src="./upload/intr/notation4.gif" alt="" ></li>
+        </ul>
+    </div>
+    <script src="./js/jquery-3.5.1.min.js"></script>
+    <script src="./js/style.js"></script>
+</body>
+</html>
---
+++ js/style.js
@@ -0,0 +1,99 @@
+var head_word=$(".head div ul li");
+var side=$(".side .sidein");
+head_word.click(
+    function()
+    {   head_word.removeClass();
+        $(this).addClass("cross_word_change");
+        var i=$(this).index('li');
+        for(let j=0;j<4;j++)
+        {
+            if(i==j)
+                side.eq(j).css("display","block");
+            else
+                side.eq(j).css("display","none");      
+        }        
+    }
+)
+//************************************************************* */ 
+var formulali1=$(".ul1 li");
+var formuladiv1=$(".ul1 li div");
+var flag1=$(".ul1 li a");
+formulali1.click(
+    function(){
+        var k=$(this).index('.ul1 li');
+        formuladiv1.css("display","none")
+        formuladiv1.eq(k).css("display","block");
+        flag1.css("background-color","transparent");
+        flag1.eq(k).css("background-color","rgb(43,48,59)");
+    }
+)
+
+var formulali2=$(".ul2 li");
+var formuladiv2=$(".ul2 li div");
+var flag2=$(".ul2 li a");
+formulali2.click(
+    function(){
+        var k=$(this).index('.ul2 li');
+        formuladiv2.css("display","none");
+        formuladiv2.eq(k).css("display","block");
+        flag2.css("background-color","transparent");
+        flag2.eq(k).css("background-color","rgb(43,48,59)");
+    }
+)
+
+var formulali3=$(".ul3 li");
+var formuladiv3=$(".ul3 li div");
+var flag3=$(".ul3 li a");
+formulali3.click(
+    function(){
+        var k=$(this).index('.ul3 li');
+        formuladiv3.css("display","none")
+        formuladiv3.eq(k).css("display","block");
+        flag3.css("background-color","transparent");
+        flag3.eq(k).css("background-color","rgb(43,48,59)");
+    }
+)
+
+var formulali4=$(".ul4 li");
+var formuladiv4=$(".ul4 li div");
+var flag4=$(".ul4 li a");
+formulali4.click(
+    function(){
+        var k=$(this).index('.ul4 li');
+        formuladiv4.css("display","none")
+        formuladiv4.eq(k).css("display","block");
+        flag4.css("background-color","transparent");
+        flag4.eq(k).css("background-color","rgb(43,48,59)");
+    }
+)
+// *********************************************************
+//魔方
+var close=false;
+$(".cube_close").click(function(){
+    if(close){
+        $(".cube").animate({"right":"-300px"},800);
+        close=false;
+    }
+    else{
+        $(".cube").animate({"right":"0"},800);
+        close=true;
+    }
+});
+$(".fresh").click(
+    function(){
+        $('.cube_html').attr('src', $('.cube_html').attr('src'));
+
+    }
+)
+//公式说明
+var close1=false;
+$(".intr_close").click(function(){
+    if(close1){
+        $(".intr").animate({"right":"-300px"},800);
+        close1=false;
+    }
+    else{
+        $(".intr").animate({"right":"-20"},800);
+        close1=true;
+    }
+});
